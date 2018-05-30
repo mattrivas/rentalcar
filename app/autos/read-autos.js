00bfd7e25114dc764b5248c88dcc52f200644e4f
JorgeVaira <jorge_vaira_95@hotmail.com>
2018-05-30 17:56:41 -0300

    Cambios, agregado el read.php

---
+++ app/autos/read-autos.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+	showAutos();
+
+});
+$(document).on('click', '.read-products-button', function(){
+	showAutos();
+});
+function showAutos(){
+}
+$.getJSON("http://localhost/api/autos/read.php", function(data){
+	var read_products_html="";
+
+	read_products_html+="<div id='create-product' class='btn btn-primary pull-right m-b-15px create-product-button'>";
+	read_products_html+="<span class='glyphicon glyphicon-plus'></span> Create Product";
+	read_products_html+="</div>";
+	read_products_html+="<table class='table table-bordered table-hover'>";
+
+	read_products_html+="<tr>";
+	read_products_html+="<th class='w-25-pct'>Name</th>";
+	read_products_html+="<th class='w-10-pct'>Price</th>";
+	read_products_html+="<th class='w-15-pct'>Category</th>";
+	read_products_html+="<th class='w-25-pct text-align-center'>Action</th>";
+	read_products_html+="</tr>";
+
+	$.each(data.records, function(key, val) {
+
+		read_products_html+="<tr>";
+
+		read_products_html+="<td>" + val.name + "</td>";
+		read_products_html+="<td>$" + val.price + "</td>";
+		read_products_html+="<td>" + val.category_name + "</td>";
+
+		read_products_html+="<td>";
+		read_products_html+="<button class='btn btn-primary m-r-10px read-one-product-button' data-id='" + val.id + "'>";
+		read_products_html+="<span class='glyphicon glyphicon-eye-open'></span> Read";
+		read_products_html+="</button>";
+
+		read_products_html+="<button class='btn btn-info m-r-10px update-product-button' data-id='" + val.id + "'>";
+		read_products_html+="<span class='glyphicon glyphicon-edit'></span> Edit";
+		read_products_html+="</button>";
+
+		read_products_html+="<button class='btn btn-danger delete-product-button' data-id='" + val.id + "'>";
+		read_products_html+="<span class='glyphicon glyphicon-remove'></span> Delete";
+		read_products_html+="</button>";
+		read_products_html+="</td>";
+
+		read_products_html+="</tr>";
+
+	});    
+	read_products_html+="</table>";
+	$("#page-content").html(read_products_html);
+});
